881f3e377f1be89199c20e90fb4d14cfe36327ce
jhirvinmago <jhirvinsanandres@gmail.com>
2024-10-07 08:53:57 +0800

    Add files via upload

---
+++ .04BasicInput.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int course, school;
+	string name;
+	
+	cout << " Nickname: ";
+	cin >> name;
+	// only number not leter in course level
+	cout << " Course-Year Level:";
+	cin >> course;
+	cout << " School: ";
+	cin >> school ;
+	cout << " wow congrats "<< name << " is a nice course. And you are studying in " << school << " which is one of the center of exellence in tertiary education " << endl;
+	return 0;
+}
+
+
+
+	
